6398e142b490a22ed4f880a612fb58b4d034e14c
Justin Barnowski <jbarno@umich.edu>
2013-09-10 20:33:01 -0400

    Set up for Justin.
    Possible discrepancy in project.properties referencing
    imlc-android-common and collabrify-client-android in this commit.

--- gen/com/pandj/wewrite/R.java
+++ gen/com/pandj/wewrite/R.java
@@ -21,7 +21,8 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
@@ -33,9 +34,12 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
+        public static final int action_settings=0x7f040005;
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040002;
+        public static final int hello_world=0x7f040001;
+        public static final int menu_settings=0x7f040002;
+        public static final int title_activity_main=0x7f040003;
+        public static final int url=0x7f040004;
     }
     public static final class style {
         /** 

3422ad0236f9440af5b9461c931fa26441587284
Justin Barnowski <jbarno@umich.edu>
2013-09-17 16:27:44 -0400

    Removed the R.java (gitignore should now take care of this I hopw) Finished the rough layouts of main activity and text editor

---
+++ src/com/pandj/wewrite/TextEditor.java
@@ -0,0 +1,26 @@
+package com.pandj.wewrite;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TextEditor extends Activity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_text_editor);
+  }
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu)
+  {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.text_editor, menu);
+    return true;
+  }
+
+}
